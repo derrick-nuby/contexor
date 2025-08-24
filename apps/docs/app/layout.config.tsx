e5e78296d2479219fdac4590695946b8bafe0f8f
Derrick [Nuby] Iradukunda <72933285+derrick-nuby@users.noreply.github.com>
2025-08-25 01:22:06 +0200

    feat: migrate documentation structure to support multilingual layouts and content; add French translations and update layout configurations

--- apps/docs/app/layout.config.tsx
+++ apps/docs/app/layout.config.tsx
@@ -4,8 +4,8 @@ import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
  * Shared layout configurations
  *
  * you can customise layouts individually from:
- * Home Layout: app/(home)/layout.tsx
- * Docs Layout: app/docs/layout.tsx
+ * Home Layout: app/[lang]/(home)/layout.tsx
+ * Docs Layout: app/[lang]/docs/layout.tsx
  */
 export const baseOptions: BaseLayoutProps = {
   nav: {
